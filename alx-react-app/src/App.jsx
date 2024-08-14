81ee53e01b7a0a23eb72fc383276baa1140de115
Abdulhakim <fedluabdulhakim.com>
2024-08-14 15:11:42 +0300

    Updated WelcomeMessage component and integrated it into App.jsxand more and more

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,5 @@
+import UserProfile from './components/UserProfile';
+
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -22,6 +24,7 @@ function App() {
         <div>
             <Header />
             <MainContent />
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
             <Footer />
         </div>
     );
