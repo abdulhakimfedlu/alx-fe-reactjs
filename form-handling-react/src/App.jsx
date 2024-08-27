67a8d703992264922f485d03a84cbb15007300e4
Abdulhakim <fedluabdulhakim.com>
2024-08-27 18:01:00 +0300

    Initial commit - Recipe Sharing App with Zustand

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -4,7 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
-
+import React from 'react';
+import FormikForm from './components/formikForm';
+function App() {
+  return (
+    <div>
+      <h1>User Registration with Formik</h1>
+      <FormikForm />
+    </div>
+  );
+}
 function App() {
   return (
     <div>
